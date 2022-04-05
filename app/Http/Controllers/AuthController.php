2fa646d3b928ce5ce506e0d245fe427cf4ec9655
Sergio Santos <josesergio.s@hotmail.com>
2022-04-05 10:46:19 -0300

    Adicionando um novo usuário ao banco

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class AuthController extends Controller
 {
@@ -16,5 +17,55 @@ class AuthController extends Controller
                 'unauthorized'
             ]
         ]);
+    }
+
+    public function create(Request $request)
+    {
+        $array = ['error'=>''];
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $birthdate = $request->input('birthdate');
+
+        if($name && $email && $password && $birthdate){
+            //Validando a data de nascimento
+            if (strtotime($birthdate) === false){
+                $array['error'] = 'Data de nascimento inválida!';
+                return $array;
+            }
+
+            //Verificar a existencia do email no banco
+            $emailExists = User::where('email', $email)->count();
+            if ($emailExists === 0){
+
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $newUser = new User();
+                $newUser->name = $name;
+                $newUser->email = $email;
+                $newUser->password = $hash;
+                $newUser->birthdate = $birthdate;
+                $newUser->save();
+
+                $token = auth()->attempt([
+                    'email'    => $email,
+                    'password' => $password
+                ]);
+                if (!$token){
+                    $array['error'] = 'Ocorreu um erro!';
+                    return $array;
+                }
+                $array['token'] = $token;
+
+            }else{
+                $array['error'] = 'E-mail já cadastrado!';
+                return $array;
+            }
+
+        }else {
+            $array['error'] = 'Não enviou todos os campos.';
+        }
+
+        return $array;
     }
 }
