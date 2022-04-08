a89ceda3f14c57b0a4dce8ded8bd07a95ecbea26
Sergio Santos <josesergio.s@hotmail.com>
2022-04-08 00:28:37 -0300

    Alteração de dados de usuários implementada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
+
 
 class UserController extends Controller
 {
@@ -20,7 +22,64 @@ class UserController extends Controller
         //PUT  api/user (name, email, birthdate, city, work, password, password_confirm)
         $array = ['error'=>''];
 
+        $name             = $request->input('name');
+        $email            = $request->input('email');
+        $birthdate        = $request->input('birthdate');
+        $city             = $request->input('city');
+        $work             = $request->input('work');
+        $password         = $request->input('password');
+        $password_confirm = $request->input('password_confirm');
 
+        $user = User::find($this->loggedUser['id']);
+
+        //NAME
+        if ($name){
+            $user->name = $name;
+        }
+
+        //EMAIL
+        if ($email){
+            if ($email != $user->email){
+                $emailExists = User::where('email', $email)->count();
+                if ($emailExists === 0){
+                    $user->email = $email;
+                }else{
+                    $array['error'] = 'E-mail já existe!';
+                    return $array;
+                }
+            }
+        }
+
+        //BIRTHDATE
+        if ($birthdate){
+            if (strtotime($birthdate) === false){
+                $array['error'] = 'Data de nascimento inválida!';
+                return $array;
+            }
+        }
+
+        //CITY
+        if ($city){
+            $user->city = $city;
+        }
+
+        //WORK
+        if ($work){
+            $user->work = $work;
+        }
+
+        //PASSWORD
+        if ($password && $password_confirm){
+            if ($password === $password_confirm){
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $user->password = $hash;
+            }else{
+                $array['error'] = 'As senhas não conferem!';
+                return $array;
+            }
+        }
+
+        $user->save();
 
         return $array;
     }
